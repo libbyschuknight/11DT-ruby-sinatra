3b65bb40d6264b48db9a970335b8af6c81a6aab9
libbyschuknight <libbysk@gmail.com>
2015-09-24 11:32:38 +1200

    Add get for task

---
+++ console-files/short_straw_console.rb
@@ -0,0 +1,45 @@
+# # psuedo-code
+
+# print out
+# welcome message
+
+puts "==== Welcome to Short Straw ===="
+puts ""
+puts "- Teacher Instructions -"
+puts "Enter the name of the task: "
+task = gets.chomp
+
+puts task
+
+
+# instructions for teachers
+
+# loop as could be more than one task
+# have exit / cancel option for teacher at this point
+
+#   ask teacher for name of task
+#   store name of task
+
+#   ask teacher for number of students
+#   store number of students
+
+#   (teacher has to assign a number to each student)
+
+#   generate random short straw number from range of 1 to number of students
+#   assign to random number
+
+#   loop for students until short staw drawn
+#     instructions for student
+#     student enters name
+#     student enters assigned number
+#     if student assigned number is equal to random number (short straw)
+#       break loop
+#       print out "you have drawn the short straw"
+#     end
+#   end
+
+#   ask teacher if they have another task
+#   if yes start loop again
+#   else quit program
+
+# end
